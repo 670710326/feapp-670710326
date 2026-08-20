56ad28f0b948d90ca66589c722aa6309f92037c8
Pomjaktorn ngakkloy <ngakkloy_p@su.ac.th>
2026-08-20 05:03:55 +0000

    first_React_App

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -2,26 +2,35 @@ import React from 'react';
 // import Footer from './components/Footer'; 
 // import Header from './components/Header';
 // import MovieList from './components/MovieList';
-import Greeting from './components/Greeting';  
-import Card from './components/Card';
-const students = [
-  { id: 1, name: 'ฝน',   year: 3 },
-  { id: 2, name: 'เต้ย', year: 2 },
-  { id: 3, name: 'มายด์', year: 4 },
+// import Greeting from './components/Greeting';  
+// import Card from './components/Card';
+import ProfileCard from './components/ProfileCard';
+import './App.css';
+
+const members = [
+  { id: 1, name: 'พรหมจักรธร เง็กคลอย', nickname: 'กฤษ',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'เล่นเกม'] },
+   { id: 2, name: 'ชัยภัทร รัตนพร', nickname: 'ปลื้ม',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['กิน', 'เล่นเกม'] },
+    { id: 2, name: 'ณัฎฌา อิ่มหนำ', nickname: 'จั๊บ',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['นอน', 'เล่นเกม'] },
 ];
 
 function App() {
   return (
-    <div>
-        <Card title="ประกาศ">
-  <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
-  <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
-</Card>
-        <h1>รายชื่อนักศึกษา</h1>
-      {students.map((s) => (
-        <Greeting key={s.id} name={s.name} year={s.year} />
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
         ))}
-   
+      </div>
     </div>
   );
 }
